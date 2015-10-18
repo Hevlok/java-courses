06130c30e1ec7b38401398265d1222446660f937
andrbee <andrbee@gmail.com>
2015-10-18 21:45:40 +0300

    Create app Calculate ver.2

---
+++ ls4/Calculate2/src/Calculator.java
@@ -0,0 +1,38 @@
+
+public class Calculator {
+	private double first;
+	private double second;
+	private String element;
+
+	public Calculator(Double f,String el,Double s){
+		this.element=el;
+		switch(element){
+		case "+":
+			this.first=f;
+			this.second=s;
+			System.out.println(first+second);
+			break;
+		case "-":
+			this.first=f;
+			this.second=s;
+			System.out.println(first-second);
+			break;
+		case "*":
+			this.first=f;
+			this.second=s;
+			System.out.println(first*second);
+			break;
+		case "/":
+			this.first=f;
+			this.second=s;
+			System.out.println(first/second);
+			break;
+		case "e":
+			this.first=f;
+			this.second=s;
+			System.out.println(Math.pow(first, second));
+			break;
+		}		
+	}	
+	
+}
---
+++ ls4/Calculate2/src/InteractRunner.java
@@ -0,0 +1,9 @@
+
+public class InteractRunner {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}

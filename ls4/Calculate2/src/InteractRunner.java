ae150dfc10ecedbba40f415b0495af44bc394dd0
andrbee <andrbee@gmail.com>
2015-10-18 23:45:02 +0300

    fixed

--- ls4/Calculate2/src/InteractRunner.java
+++ ls4/Calculate2/src/InteractRunner.java
@@ -3,22 +3,33 @@ import java.util.Scanner;
 public class InteractRunner {
 
 	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);		
+		Scanner sc = new Scanner(System.in);
 		double first;
 		double second;
-		
 		String operation;
-		String s="NO";
-		//System.out.printlsn("Enter first value !");
-		while(s.equals("NO")){
-			System.out.println("Enter first value !");			
-			first=sc.nextDouble();
-			System.out.println("Enter operation!");
-			operation=sc.next();
-			System.out.println("Enter second value !");
-			second=sc.nextDouble();
-			result=new Calculator(first, operation, second);
-			
+		String s = "NO";
+		
+		try {
+			while (s.equals("NO")) {
+				System.out.println("Enter first value !"+"\r");
+				first = sc.nextDouble();
+				System.out.println("Enter operation!"+"\r");
+				operation = sc.next();
+				System.out.println("Enter second value !"+"\r");
+				second = sc.nextDouble();
+				new Calculator(first, operation, second);
+				System.out.println("Do you want exit ?"+"\r");
+				System.out.println("Press 'Y' to exit / Press 'N' to continue !"+"\r");
+				if(sc.next().equals("[yY]")) break;
+				else if(sc.next().equals("[nN]"))
+					System.out.println("Clear 'result' ?"+"\r");
+					break;
+				
+				
+
+			}
+		} finally {
+			sc.close();
 		}
 
 	}

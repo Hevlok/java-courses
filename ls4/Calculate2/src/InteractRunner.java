3b193b67e3b2ce7b492fec43148f8abdfac7c763
andrbee <andrbee@gmail.com>
2015-10-18 23:08:19 +0300

    create InteractRunner

--- ls4/Calculate2/src/InteractRunner.java
+++ ls4/Calculate2/src/InteractRunner.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
 
 public class InteractRunner {
 
 	public static void main(String[] args) {
-		
+		Scanner sc= new Scanner(System.in);		
+		double first;
+		double second;
+		String operation;
+		String s="NO";
+		//System.out.printlsn("Enter first value !");
+		while(s.equals("NO")){
+			System.out.println("Enter first value !");			
+			first=sc.nextDouble();
+			System.out.println("Enter operation!");
+			operation=sc.next();
+			System.out.println("Enter second value !");
+			second=sc.nextDouble();
+			new Calculator(first, operation, second);
+			
+		}
 
 	}
 

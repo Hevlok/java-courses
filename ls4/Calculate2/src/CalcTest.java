f2532e1ea67e8b44f07c4146f130e231b19be05c
andrbee <andrbee@gmail.com>
2015-11-08 00:01:01 +0300

    UTF-8 encoding

--- ls4/Calculate2/src/CalcTest.java
+++ ls4/Calculate2/src/CalcTest.java
@@ -10,21 +10,30 @@ public class CalcTest {
 		Calculator test=new Calculator();
 		test.setFirst(1);
 		test.setSecond(2);
+		
 		test.setOperation("+");
 		double resultSumm=test.calc();
 		assertEquals(3,resultSumm,0.0);
+		
 		test.setOperation("-");
 		double resultRazn=test.calc();
 		assertEquals(-1,resultRazn,0.0);
+		
 		test.setOperation("/");
 		double resultDel=test.calc();
 		assertEquals(0.5,resultDel,0.0);
+		
 		test.setOperation("*");
 		double resultUmn=test.calc();
 		assertEquals(2,resultUmn,0.0);
+		
 		test.setOperation("e");
 		double resultPow=test.calc();
 		assertEquals(1,resultPow,0.0);
+		
+		test.setOperation(".");
+		double resultErr=test.calc();
+		assertEquals(0,resultErr,0.0);
 	}
 
 }
